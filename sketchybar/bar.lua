4ce2e684da49a8b75cf7467ff4d23765704252f7
Dhruv Arora <a_dhruv@outlook.com>
2025-07-24 17:57:50 -0700

    feat: adding new sketchybar

---
+++ sketchybar/bar.lua
@@ -0,0 +1,10 @@
+local colors = require("colors")
+
+-- Equivalent to the --bar domain
+sbar.bar({
+  height = 32,
+  margin = 12,
+  y_offset = 3,
+  corner_radius = 12,
+  color = colors.bar.bg,
+})
